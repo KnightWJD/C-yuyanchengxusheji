38a4e5eadbc682ce39f40c8316f36cd9b72a6eb1
knightWJD <2630225285@qq.com>
2021-03-04 10:58:49 +0800

    求两个数之和

---
+++ 1.2.c
@@ -0,0 +1,13 @@
+// 求两个数之和
+
+#include <stdio.h>
+
+int main()
+{
+    int a = 0;
+    int b = 0;
+    printf("请输入2个数字\n");
+    scanf("%d%d",&a,&b);
+    printf("%d+%d = %d\n",a,b,a+b);
+    return 0;
+}
