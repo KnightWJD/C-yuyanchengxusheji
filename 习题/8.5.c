451ae5a10e1780786fdc99318934c66a2b112a35
KnightWJD <64474090+KnightWJD@users.noreply.github.com>
2022-03-03 10:14:45 +0800

    报到3的人退出圈子

--- 习题/8.5.c
+++ 习题/8.5.c
@@ -4,32 +4,59 @@
 #include <stdio.h>
 
 
-#define n 10
-int array_final[n]={0};
+#define n 15
+int array_final[n]={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
 int j;
-void disposal(int *p)
+
+
+int disposal(int *p,int len,int count,int k)
 {
+
     int i;
-    for(i=0,j=0;i<n;i++)
+    if(len ==1)
     {
-        if(*(p+i)%3==0)
+       return array_final[0];
+    }
+    for(i=0,j=0;i<len;i++)
+    {
+        if(count == 0)
         {
-            continue;
+            if((i+1)%3==0)
+            {
+                continue;
+            }
         }
+        if(count !=0)
+        {
+            if((i+k+1)%3==0)
+            {
+                continue;
+            }
+        }
+
         array_final[j]=*(p+i);
         j++;
     }
+
+    k = len%3+k;
+    if(k>2)
+    {
+        k = k%3;
+    }
+
+
+    len = j;
+    count++;
+
+    disposal(array_final,len,count,k);
 }
 
 int main()
 {
-    int array[n]={1,2,3,4,5,6,7,8,9,10};
-    disposal(array);
-    printf("最后剩下的人分别是:\n");
-    int i;
-    for(i=0;i<j;i++)
-    {
-        printf("第 %d 号\n",array_final[i]);
-    }
+
+    int count=0;
+    int k=0;
+    int final = disposal(array_final,n,count,k);
+    printf("最后剩下的人是:  %d 号\n",final);
     return 0;
 }
